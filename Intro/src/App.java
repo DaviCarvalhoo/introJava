58065767daf699dcfd9618a42a0a358ed54be759
DaviCarvalhoo <davicarvalhotech@gmail.com>
2023-08-23 20:20:51 -0300

    update

---
+++ Intro/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args){
+        String meuNome = "Davi";
+        int anoInicio = 2023;
+        boolean java = true;
+        anoInicio = 2022;
+        System.out.println("Meu nome é: ");
+    }
+}

77927be163acf892c6450dee4bd2a751212a99a6
DaviCarvalhoo <davicarvalhotech@gmail.com>
2023-08-23 20:21:05 -0300

    update

--- sintaxe/src/App.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class App {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        String nome = scan.nextLine();
-        System.out.println(nome);
-    }
-}

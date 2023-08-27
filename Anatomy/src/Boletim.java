1c80691bbd8d589ffebd8d8f35aaac2658db9fbf
DaviCarvalhoo <davicarvalhotech@gmail.com>
2023-08-27 20:14:49 -0300

    create if

--- Anatomy/src/Boletim.java
+++ Anatomy/src/Boletim.java
@@ -1,5 +1,12 @@
 public class Boletim {
 public static void main(String[] args) {
-        
+        int mediaFinal = 5;
+
+        if (mediaFinal > 6)
+            System.out.println("Aprovado");
+        else if (mediaFinal == 6)
+            System.out.println("Prova final");
+        else 
+            System.out.println("Reprovado");
 }
 }

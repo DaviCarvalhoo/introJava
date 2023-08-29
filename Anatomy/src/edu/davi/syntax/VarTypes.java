81e6722bd06440a677f040211b97904011ae125a
DaviCarvalhoo <davicarvalhotech@gmail.com>
2023-08-29 19:45:50 -0300

    study var types

---
+++ Anatomy/src/edu/davi/syntax/VarTypes.java
@@ -0,0 +1,12 @@
+package edu.davi.syntax;
+public class VarTypes {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeNormal = numeroCurto;
+        short numeroCurto2 = (short) numeNormal;
+
+        int numeroFinal = 1;
+        final int NUMERO_FINAL = 2;
+    }
+}
